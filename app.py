acd6d2dc1339024c1f59e5d871128740540a04e3
당근을 흔들어라 <76269640+qwqw82000@users.noreply.github.com>
2023-08-29 15:59:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,18 +7,22 @@ def main():
     name = st.text_input("이름 입력", "뤼튼")
 
     # 버튼 클릭 이벤트 처리
-    if st.button("인사"):
-        st.write(f"안녕하세요, {name}님!")
+    if st.button("확인"):
+        st.write(f"{name}학생에 대한 평가를 시작합니다!")
     # Radio 버튼 생성
-    option = st.radio("선택하세요", ("옵션 1", "옵션 2", "옵션 3"))
+    option = st.radio("선택하세요", ("1점", "2점", "3점", "4점"))
     
     # 선택된 옵션에 따라 동작 수행
-    if option == "옵션 1":
-        st.write("옵션 1이 선택되었습니다.")
-    elif option == "옵션 2":
-        st.write("옵션 2가 선택되었습니다.")
+    if option == "1점":
+        st.write("1점이 선택되었습니다.")
+    elif option == "2점":
+        st.write("2점가 선택되었습니다.")
+    elif option == "3점":
+        st.write("3점이 선택되었습니다.")
+    elif option == "4점":
+        st.write("4점이 선택되었습니다.")
     else:
-        st.write("옵션 3이 선택되었습니다.")
+        st.write("5점이 선택되었습니다.")
 
 if __name__ == "__main__":
     main()
